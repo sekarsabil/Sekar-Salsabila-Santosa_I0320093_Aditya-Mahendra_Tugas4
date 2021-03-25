78021c5109ca85fc9ef1e8c070235387ada8e5d3
Sekar Salsabila <sekarsalsabilaaa@gmail.com>
2021-03-25 21:39:03 +0700

    Update

--- I0320093_soal6_tugas4.py
+++ I0320093_soal6_tugas4.py
@@ -1,18 +1,18 @@
 # Aplikasi Operator Bitwise
-a = 4
-b = 11
+a = 4                       # 4 = 0100
+b = 11                      # 11 =1011
 
 # 4 | 11
 Hasil = a | b
-print('Hasil dari a | b adalah ', Hasil, 'Binary = ', format(Hasil,'08b'))
+print('Hasil dari a | b adalah ', Hasil, 'Binary = ', format(Hasil,'07b'))
 
 # 4 >> 11
 Hasil = a >> b
-print('Hasil dari a >> b adalah ', Hasil, 'Binary = ', format(Hasil,'08b'))
+print('Hasil dari a >> b adalah ', Hasil, 'Binary = ', format(Hasil,'07b'))
 
 # 4 ^ 11
 Hasil = a ^ b
-print('Hasil dari a * b adalah ', Hasil, 'Binary = ', format(Hasil,'08b'))
+print('Hasil dari a * b adalah ', Hasil, 'Binary = ', format(Hasil,'07b'))
 
 # ~a
 Hasil = ~a
@@ -20,4 +20,4 @@ print('Hasil dari ~a adalah ', Hasil, 'Binary = ', format(Hasil,'08b'))
 
 # 11 & 4
 Hasil = b & a
-print('Hasil dari b & a adalah ', Hasil, 'Binary = ', format(Hasil,'08b'))
+print('Hasil dari b & a adalah ', Hasil, 'Binary = ', format(Hasil,'07b'))
